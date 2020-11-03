2421e2fdb3118dd2aeb9771070db31245a2b0bc3
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2020-11-03 14:53:23 +0100

    page d'accueil

--- index.php
+++ index.php
@@ -1,7 +1,63 @@
 <?php include './parts/head.php'; ?>
 <?php include './parts/header.php'; ?>
 
-PAGE D'ACCUEIL
+
+
+<div class="resume_map">
+    <h2 id="title">Nantes d’hier à aujourd’hui :</h2>
+    <p id="resume">
+
+        Progressivement Nantes a permis de s’intégrer comme ville connectée et écologique.
+        Cette transformation, s’est accompagnée par une modification des modes de vie : le télétravail ou les potagers solidaires.
+
+        Zoom sur l’évolution de la ville au cours de ces dernières  années :
+        > développement des pistes cyclables, des centres de tri, des points de composts etc.
+
+        A travers cette carte, visualisez la transformation Nantaise !
+    </p>
+
+    <figure>
+        <img id="map_today" src="src/ressources/Nantes.PNG" alt="Nantes aujourd'hui" >
+        <figcaption>Nantes aujourd'hui</figcaption>
+    </figure>
+
+    <figure>
+        <img id="map_tomorrow" src="src/ressources/Nantes.PNG" alt="Nantes demain">
+        <figcaption>Nantes demain</figcaption>
+    </figure>
+</div>
+
+<div class="around_you">
+   <h2>Autour de vous:</h2>
+
+    <p>Afin de vous accompagner dans vos nouveaux modes de vie, nous avons concentré ici, les indispensables de votre quotidien.</p>
+</div>
+
+<div class="lien">
+    <h4 id="velo_title">Nantes à vélo :</h4>
+    <p id="velo_resume">grâce au bicloo, empruntez les pistes cyclables. </p>
+    <img src="src/ressources/velo.jpg" id="velo_img">
+    <h4 id="potager_title">Potagers partagés & solidaires :</h4>
+    <p id="potager_resume">profitez des avantages de la campagne, en pleine ville.</p>
+    <img src="src/ressources/potager.jpg" id="potager_img">
+    <h4 id="compost_title">Réduisez vos déchets :</h4>
+    <p id="compost_resume">   localisez les composts, près de chez vous. </p>
+    <img src="src/ressources/compost.jpg" id="compost_img">
+
+</div>
+
+<div class="all_actors">
+    <h5 id="actors_title">Devenez à votre tour, acteur de la transition écologique de votre ville !</h5>
+    <p id="actors_resume">Accédez à notre réseau de proximité. Nous avons regroupé les contacts associatifs, qui oeuvrent quotidiennement à Nantes. Ils ont besoin de votre contribution. </p>
+</div>
+
+<div class="flux">
+    <h5 id="agendas_title">A vos agendas</h5>
+    <h5 id="nouvelles_title">Les dernières nouvelles</h5>
+
+</div>
+
+
 
 
 
