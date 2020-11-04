6055ea0263ab73c653f18b57a962e262ee6e427e
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2020-11-04 16:51:10 +0100

    Modif avant pull

--- contact.php
+++ contact.php
@@ -1,7 +1,28 @@
 <?php include './parts/head.php'; ?>
 <?php include './parts/header.php'; ?>
 
-PAGE DE CONTACT
+<div class="formulaire">
+    <div id="nom">
+        <label for="nom_text">Nom :</label>
+        <input type="text" id="nom_text">
+    </div>
+    <div id="prenom">
+        <label for="prenom_text">Prenom :</label>
+        <input type="text" id="prenom_text">
+    </div>
+    <div id="mail">
+        <label for="mail_text">Mail :</label>
+        <input type="email" id="mail_text">
+    </div>
+    <div id="objet">
+        <label for="objet_text">Objet :</label>
+        <input type="text" id="objet_text">
+    </div>
+    <div id="message">
+        <label for="message_text">Votre message :</label>
+        <textarea rows="15" cols="40" id="message_text"></textarea>
+    </div>
+</div>
 
 
 
