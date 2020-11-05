87b53116bc0e619f463d65c7ed6cff05825dc8f4
Theo Zaranski <theo.zaranski@gmail.com>
2020-11-05 14:36:13 +0100

    ajout formulaire page contact

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php include './parts/head.php'; ?>
+<?php include './parts/header.php'; ?>
+
+<div class="container contact-form">
+    <div class="contact-image">
+        <img src="src/img/Logo_workshopBlack.png" height="200px" alt="rocket_contact"/>
+    </div>
+    <form method="post">
+        <h3>Contactez nous</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="text" name="txtName" class="form-control" placeholder="Nom *" value="" />
+                </div>
+                <div class="form-group">
+                    <input type="text" name="txtEmail" class="form-control" placeholder="Email *" value="" />
+                </div>
+                <div class="form-group">
+                    <input type="text" name="txtPhone" class="form-control" placeholder="Objet *" value="" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="btnSubmit" class="btnContact" value="Envoyer" />
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <textarea name="txtMsg" class="form-control" placeholder="Intitulé du message *" style="width: 100%; height: 150px;"></textarea>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
--- map.php
+++ map.php
@@ -1,7 +1,7 @@
 <?php include './parts/head.php'; ?>
 <?php include './parts/header.php';
 
-$db = new PDO("mysql:host=localhost" . ";dbname=wordpress2020", "root", "root");
+$db = new PDO("mysql:host=localhost" . ";dbname=workshop2020", "root", "");
 
 
 $r = $db->prepare("select * from Position ");
@@ -18,10 +18,12 @@ $positions = $r->fetchAll();
     function initMap() {
 
         // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
+        //47.254384826
+        //-1.570838214
+        const uluru = { lat: 47.254384826, lng: -1.570838214 };
         // The map, centered at Uluru
         const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 4,
+            zoom: 12,
             center: uluru,
         });
         <?php
